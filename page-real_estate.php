719908e7071a728b020545c4830037f208b0420d
pandablue0809 <bluep0415@gmail.com>
2024-11-18 16:15:15 +0900

    add real estate pge

---
+++ page-real_estate.php
@@ -0,0 +1,213 @@
+<?php get_header()?>
+
+<div class="p-main" id="real_estate">
+        <div class="p-nav">
+            <div class="__container">
+                <a href="#">ホーム &nbsp;&nbsp;&nbsp; ></a>
+                <a href="#">事業ドメイン &nbsp;&nbsp;&nbsp; ></a>
+                <a href="#">不動産事業</a>
+            </div>
+        </div>
+        <div class="p-pagettl">
+            <div class="__container">
+
+                <p class="en-ttl wow fadeInUp">
+                    BUSINESS DOMAIN
+                </p>
+                <h3 class="jp-ttl wow fadeInUp">
+                    不動産事業
+                </h3>
+                <figure class="__absolute wow fadeInRight is-pc">
+                    <img src="<?php echo get_template_directory_uri() ?>/assets/img/about/img-ttl.png" alt="">
+                </figure>
+                <figure class="__absolute wow fadeInRight is-sp">
+                    <img src="<?php echo get_template_directory_uri() ?>/assets/img/real_estate/img-absoulute@2x.png" alt="">
+                </figure>
+            </div>
+        </div>
+
+        <div class="p-intro">
+            <div class="__intro-ctn wow fadeInLeft">
+                <p>フロンティアグループは、不動産を資産から運用へと導くアセットコンサルティング、中古物件を仕入れ・リニューアルして再販する再生事業、収益性の高い物件を取得・賃貸管理する買収・賃貸事業を展開。各部門が連携し、不動産資産の価値を最大化する柔軟なソリューションを提供しています。
+                </p>
+            </div>
+        </div>
+
+        <div class="p-asset">
+            <div class="__container">
+                <div class="__asset-item">
+                    <div class="__asset-ctn wow fadeInUp">
+                        <div class="__asset-ttl">
+                            <p class="en-ttl">
+                                Asset Consulting
+                            </p>
+                            <h3 class="jp-ttl">
+                                アセットコンサルティング部門
+                            </h3>
+                        </div>
+                        <figure class="__asset-img is-sp">
+                            <img src="<?php echo get_template_directory_uri() ?>/assets/img/real_estate/img-asset_1.jpg" alt="">
+                        </figure>
+                        <p class="__asset-desc is-pc  wow fadeInUp">
+                            不動産は、所有するだけでなく、いかに運用するかが重要な時代です。個人や法人の資産形成において、不動産の運用や戦略的活用が中・長期的な資産の差を生むようになっています。不動産を取り巻く経済環境は日々変化しており、所有者には専門的かつ柔軟な提案が求められています。<br><br>
+                            フロンティアグループの不動産アセットコンサルティングは、豊富な実績とノウハウを活かし、時代を先取りしたスキームを提供します。当部門では、不動産に関するあらゆるニーズに対応し、ワンステップでソリューションを提供する総合力が強みです。
+                        </p>
+                    </div>
+                    <figure class="__asset-img is-pc wow fadeInRight">
+                        <img src="<?php echo get_template_directory_uri() ?>/assets/img/real_estate/img-asset_1.jpg" alt="">
+                    </figure>
+                    <p class="__asset-desc is-sp wow fadeInUp">
+                        不動産は、所有するだけでなく、いかに運用するかが重要な時代です。個人や法人の資産形成において、不動産の運用や戦略的活用が中・長期的な資産の差を生むようになっています。不動産を取り巻く経済環境は日々変化しており、所有者には専門的かつ柔軟な提案が求められています。<br><br>
+                        フロンティアグループの不動産アセットコンサルティングは、豊富な実績とノウハウを活かし、時代を先取りしたスキームを提供します。当部門では、不動産に関するあらゆるニーズに対応し、ワンステップでソリューションを提供する総合力が強みです。
+                    </p>
+                </div>
+                <div class="__asset-item __reverse">
+                    <div class="__asset-ctn">
+                        <div class="__asset-ttl">
+                            <p class="en-ttl">
+                                Recycle and Renovation
+                            </p>
+                            <h3 class="jp-ttl">
+                                再生・リニューアル部門
+                            </h3>
+                        </div>
+                        <p class="__asset-desc is-pc">
+                            再生・リニューアル部門では、中古マンションや中古戸建を個別に仕入れ、リニューアルした上で販売しています。また、仕入れた中古戸建を解体し、戸建用地や新築戸建として再販売します。物件の仕入れは、大手不動産仲介会社、弁護士事務所、税理士事務所、サービサー、金融機関など、独自のネットワークを活用して行います。<br><br>
+                            中古物件は内装や間取り、設備を予算内で見直し、洗練された住居に再生した後、販売します。対象エリアは、東京都および隣接3県で、駅から徒歩9分以内、平成築以降の物件を金額上限無しで買い取ります。
+                        </p>
+                        <div class="__asset-detail is-pc  wow fadeInUp">
+                            <p class="__detail-ctn">
+                                〈区分マンション・戸建　買取実績〉
+                            </p>
+                            <div class="__detail-items">
+                                <div class="__detail-item">
+                                    <p class="year">
+                                        2019
+                                    </p>
+                                    <p class="number">
+                                        42<span>物件</span>
+                                    </p>
+                                </div>
+                                <div class="__detail-item">
+                                    <p class="year">
+                                        2020
+                                    </p>
+                                    <p class="number">
+                                        54<span>物件</span>
+                                    </p>
+                                </div>
+                                <div class="__detail-item">
+                                    <p class="year">
+                                        2021
+                                    </p>
+                                    <p class="number">
+                                        52<span>物件</span>
+                                    </p>
+                                </div>
+                                <div class="__detail-item">
+                                    <p class="year">
+                                        2022
+                                    </p>
+                                    <p class="number">
+                                        54<span>物件</span>
+                                    </p>
+                                </div>
+                                <div class="__detail-item">
+                                    <p class="year">
+                                        2023
+                                    </p>
+                                    <p class="number">
+                                        52<span>物件</span>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <figure class="__asset-img is-sp  wow fadeInUp">
+                            <img src="<?php echo get_template_directory_uri() ?>/assets/img/real_estate/img-asset_2.jpg" alt="">
+                        </figure>
+
+                    </div>
+                    <figure class="__asset-img is-pc  wow fadeInLeft">
+                        <img src="<?php echo get_template_directory_uri() ?>/assets/img/real_estate/img-asset_2.jpg" alt="">
+                    </figure>
+                    <p class="__asset-desc is-sp  wow fadeInUp">
+                        再生・リニューアル部門では、中古マンションや中古戸建を個別に仕入れ、リニューアルした上で販売しています。また、仕入れた中古戸建を解体し、戸建用地や新築戸建として再販売します。物件の仕入れは、大手不動産仲介会社、弁護士事務所、税理士事務所、サービサー、金融機関など、独自のネットワークを活用して行います。<br><br>
+                        中古物件は内装や間取り、設備を予算内で見直し、洗練された住居に再生した後、販売します。対象エリアは、東京都および隣接3県で、駅から徒歩9分以内、平成築以降の物件を金額上限無しで買い取ります。
+                    </p>
+                    <div class="__asset-detail is-sp  wow fadeInUp">
+                        <p class="__detail-ctn">
+                            〈区分マンション・戸建　買取実績〉
+                        </p>
+                        <div class="__detail-items">
+                            <div class="__detail-item">
+                                <p class="year">
+                                    2019
+                                </p>
+                                <p class="number">
+                                    42<span>物件</span>
+                                </p>
+                            </div>
+                            <div class="__detail-item">
+                                <p class="year">
+                                    2020
+                                </p>
+                                <p class="number">
+                                    54<span>物件</span>
+                                </p>
+                            </div>
+                            <div class="__detail-item">
+                                <p class="year">
+                                    2021
+                                </p>
+                                <p class="number">
+                                    52<span>物件</span>
+                                </p>
+                            </div>
+                            <div class="__detail-item">
+                                <p class="year">
+                                    2022
+                                </p>
+                                <p class="number">
+                                    54<span>物件</span>
+                                </p>
+                            </div>
+                            <div class="__detail-item">
+                                <p class="year">
+                                    2023
+                                </p>
+                                <p class="number">
+                                    52<span>物件</span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="__asset-item">
+                    <div class="__asset-ctn">
+                        <div class="__asset-ttl">
+                            <p class="en-ttl">
+                                Purchase and Rent
+                            </p>
+                            <h3 class="jp-ttl">
+                                売買・賃貸部門
+                            </h3>
+                        </div>
+                        <p class="__asset-desc is-pc  wow fadeInUp">
+                            当部門では、収益性のある不動産を積極的に取得し、長期保有を目的とした管理を行っています。自社保有不動産の賃貸管理や、プロジェクトで一時取得した販売用物件の管理も担っています。現在、アセット総額333億円を目標に、駅近の収益物件を中心に、店舗・事務所・ソシアル施設・居住系など、幅広い形態の物件を購入しています。
+                        </p>
+                        <figure class="__asset-img is-sp  wow fadeInUp">
+                            <img src="<?php echo get_template_directory_uri() ?>/assets/img/real_estate/img-asset_3.jpg" alt="">
+                        </figure>
+                    </div>
+                    <figure class="__asset-img is-pc  wow fadeInRight">
+                        <img src="<?php echo get_template_directory_uri() ?>/assets/img/real_estate/img-asset_3.jpg" alt="">
+                    </figure>
+                    <p class="__asset-desc is-sp  wow fadeInUp">
+                        当部門では、収益性のある不動産を積極的に取得し、長期保有を目的とした管理を行っています。自社保有不動産の賃貸管理や、プロジェクトで一時取得した販売用物件の管理も担っています。現在、アセット総額333億円を目標に、駅近の収益物件を中心に、店舗・事務所・ソシアル施設・居住系など、幅広い形態の物件を購入しています。
+                    </p>
+                </div>
+            </div>
+        </div>
+
+
+<?php get_footer() ?>
