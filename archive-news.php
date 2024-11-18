35790996e6f41cff6afe59cf80b0ac1f36da3e68
pandablue0809 <bluep0415@gmail.com>
2024-11-18 16:21:19 +0900

    add news page

---
+++ archive-news.php
@@ -0,0 +1,144 @@
+<?php get_header()?>
+
+<div class="p-main" id="news">
+        <div class="p-nav">
+            <div class="__container">
+                <a href="#">Home &nbsp;&nbsp;&nbsp; ></a>
+                <a href="#">NEWS</a>
+            </div>
+        </div>
+        <div class="p-pagettl">
+            <div class="__container">
+                <p class="en-ttl wow fadeInUp">
+                    NEWS
+                </p>
+                <h3 class="jp-ttl wow fadeInUp">
+                    お知らせ
+                </h3>
+                <figure class="__absolute wow fadeInRight is-pc">
+                    <img src="<?php echo get_template_directory_uri() ?>/assets/img/about/img-ttl.png" alt="">
+                </figure>
+                <figure class="__absolute wow fadeInRight is-sp">
+                    <img src="<?php echo get_template_directory_uri() ?>/assets/img/real_estate/img-absoulute@2x.png" alt="">
+                </figure>
+            </div>
+        </div>
+
+        <div class="p-newsmain">
+            <div class="__container">
+                <div class="p-newsmain-mainblock">
+                    <div class="__newsmain-item wow fadeIn">
+                        <p class="date">2024年09月24日</p>
+                        <a href="#" class="__item-ttl">
+                            「らくたま」の評判と口コミに関するブログ記事のご紹介
+                        </a>
+                        <div class="__item-cat_list">
+                            <a href="#">Blog</a>
+                            <a href="#">成功事例</a>
+                            <a href="#">不動産事業</a>
+                        </div>
+                    </div>
+                    <div class="__newsmain-item wow fadeIn">
+                        <p class="date">2024年09月24日</p>
+                        <a href="#" class="__item-ttl">
+                            「らくたま」の評判と口コミに関するブログ記事のご紹介
+                        </a>
+                        <div class="__item-cat_list">
+                            <a href="#">Blog</a>
+                            <a href="#">成功事例</a>
+                            <a href="#">不動産事業</a>
+                        </div>
+                    </div>
+                    <div class="__newsmain-item wow fadeIn">
+                        <p class="date">2024年09月24日</p>
+                        <a href="#" class="__item-ttl">
+                            「らくたま」の評判と口コミに関するブログ記事のご紹介
+                        </a>
+                        <div class="__item-cat_list">
+                            <a href="#">Blog</a>
+                            <a href="#">成功事例</a>
+                            <a href="#">不動産事業</a>
+                        </div>
+                    </div>
+                    <div class="__newsmain-item wow fadeIn">
+                        <p class="date">2024年09月24日</p>
+                        <a href="#" class="__item-ttl">
+                            「らくたま」の評判と口コミに関するブログ記事のご紹介
+                        </a>
+                        <div class="__item-cat_list">
+                            <a href="#">Blog</a>
+                            <a href="#">成功事例</a>
+                            <a href="#">不動産事業</a>
+                        </div>
+                    </div>
+                    <div class="__newsmain-item wow fadeIn">
+                        <p class="date">2024年09月24日</p>
+                        <a href="#" class="__item-ttl">
+                            「らくたま」の評判と口コミに関するブログ記事のご紹介
+                        </a>
+                        <div class="__item-cat_list">
+                            <a href="#">Blog</a>
+                            <a href="#">成功事例</a>
+                            <a href="#">不動産事業</a>
+                        </div>
+                    </div>
+                    <div class="__newsmain-item wow fadeIn">
+                        <p class="date">2024年09月24日</p>
+                        <a href="#" class="__item-ttl">
+                            「らくたま」の評判と口コミに関するブログ記事のご紹介
+                        </a>
+                        <div class="__item-cat_list">
+                            <a href="#">Blog</a>
+                            <a href="#">成功事例</a>
+                            <a href="#">不動産事業</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="p-newsmain-rightblock wow fadeInRight">
+                    <div class="__sade-cat">
+                        <h3 class="__cat-ttl">
+                            カテゴリー
+                        </h3>
+                        <div class="__cat-body">
+                            <a href="#" class="__cat-item">Blog</a>
+                            <a href="#" class="__cat-item">成功事例</a>
+                            <a href="#" class="__cat-item">不動産事業</a>
+                        </div>
+                        <div class="__item-collapse__mark is-sp">
+                            <div class="__item-collapse__mark-container">
+                                <span></span>
+                                <span></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="__sade-cat">
+                        <h3 class="__cat-ttl">
+                            アーカイブ
+                        </h3>
+                        <div class="__cat-body">
+                            <a href="#" class="__cat-item">2024年</a>
+                            <a href="#" class="__cat-item">2023年</a>
+                            <a href="#" class="__cat-item">2022年</a>
+                            <a href="#" class="__cat-item">2021年</a>
+                            <a href="#" class="__cat-item">2020年</a>
+                            <a href="#" class="__cat-item">2019年</a>
+                            <a href="#" class="__cat-item">2018年</a>
+                            <a href="#" class="__cat-item">2017年</a>
+                            <a href="#" class="__cat-item">2016年</a>
+                            <a href="#" class="__cat-item">2015年</a>
+                            <a href="#" class="__cat-item">2014年</a>
+                            <a href="#" class="__cat-item">2013年</a>
+                        </div>
+                        <div class="__item-collapse__mark is-sp">
+                            <div class="__item-collapse__mark-container">
+                                <span></span>
+                                <span></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+<?php get_footer() ?>
