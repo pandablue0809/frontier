8ec707ce3ff80a40ae560bf10c85971c7712ca32
pandablue0809 <bluep0415@gmail.com>
2024-11-18 15:53:42 +0900

    add company page

---
+++ page-company.php
@@ -0,0 +1,294 @@
+<?php get_header()?>
+
+<div class="p-main" id="company">
+        <div class="p-nav">
+            <div class="__container">
+                <a href="#">Home &nbsp;&nbsp;&nbsp; ></a>
+                <a href="#">会社情報</a>
+            </div>
+        </div>
+        <div class="p-pagettl">
+            <div class="__container">
+
+                <p class="en-ttl wow fadeIn">
+                    COMPANY INFORMATION
+                </p>
+                <h3 class="jp-ttl wow fadeIn">
+                    会社情報
+                </h3>
+                <figure class="__absolute wow fadeInRight is-pc">
+                    <img src="<?php echo get_template_directory_uri() ?>/assets/img/about/img-ttl.png" alt="">
+                </figure>
+                <figure class="__absolute wow fadeInRight is-sp">
+                    <img src="<?php echo get_template_directory_uri() ?>/assets/img/real_estate/img-absoulute@2x.png" alt="">
+                </figure>
+            </div>
+        </div>
+
+        <div class="p-company">
+            <div class="__container">
+                <div class="__company-item wow fadeIn">
+                    <p class="_c-ttl">
+                        商号
+                    </p>
+                    <div class="_c-ctn">
+                        <p>株式会社フロンティアグループ</p>
+                    </div>
+                </div>
+                <div class="__company-item wow fadeIn">
+                    <p class="_c-ttl">
+                        所在地
+                    </p>
+                    <div class="_c-ctn is-pc">
+                        <p>本店：東京都千代田区外神田5-2-5 フロンティア外神田ビル</p>
+                        <p>支店：大阪府大阪市生野区新今里1-5-3 フロンティアエグゼ今里</p>
+                    </div>
+                    <div class="_c-ctn is-sp">
+                        <div class="__flex">
+                            <p class="__ttl">本店：</p>
+                            <p>東京都千代田区外神田5-2-5 <br> フロンティア外神田ビル</p>
+                        </div>
+                        <div class="__flex">
+                            <p class="__ttl">支店：</p>
+                            <p>大阪府大阪市生野区新今里1-5-3 <br> フロンティアエグゼ今里</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="__company-item wow fadeIn">
+                    <p class="_c-ttl">
+                        設立
+                    </p>
+                    <div class="_c-ctn">
+                        <p>2008年8月8日 (設立17期目、7月本決算)</p>
+                    </div>
+                </div>
+                <div class="__company-item wow fadeIn">
+                    <p class="_c-ttl">
+                        創業者・代表者
+                    </p>
+                    <div class="_c-ctn">
+                        <p>金子 嘉德</p>
+                    </div>
+                </div>
+                <div class="__company-item wow fadeIn">
+                    <p class="_c-ttl">
+                        役員
+                    </p>
+                    <div class="_c-ctn">
+                        <p>取締役 金子 嘉德、向山 一成、宮崎 正堂</p>
+                        <p>監査役 平野 克典</p>
+                    </div>
+                </div>
+                <div class="__company-item wow fadeIn">
+                    <p class="_c-ttl">
+                        資本金
+                    </p>
+                    <div class="_c-ctn">
+                        <p>1億円</p>
+                    </div>
+                </div>
+                <div class="__company-item wow fadeIn">
+                    <p class="_c-ttl">
+                        事業内容
+                    </p>
+                    <div class="_c-ctn">
+                        <p>M&Aアドバイザリー・投資事業</p>
+                        <p>M&不動産賃貸業</p>
+                        <p>M&不動産の取得、販売、及び仲介事業</p>
+                        <p>M&不動産クラウドファンディング事業 <br class="is-sp"> (『らくたま』サイトの運営)</p>
+                        <p>再生可能エネルギー源を利用した発電事業</p>
+
+                        <div class="star">
+                            <p class="_star-ttl">
+                                備考：
+                            </p>
+                            <p class="_star-ctn">
+                                全ての事業部において案件のご紹介で成約に至った場合、紹介手数料または仲介<br>
+                                手数料をお支払い致します。情報、そしてネットワークを大切にします。
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="__company-item wow fadeIn">
+                    <p class="_c-ttl">
+                        監査法人
+                    </p>
+                    <div class="_c-ctn">
+                        <p>あおい監査法人</p>
+                    </div>
+                </div>
+                <div class="__company-item wow fadeIn">
+                    <p class="_c-ttl">
+                        従業員
+                    </p>
+                    <div class="_c-ctn">
+                        <p>18名 (子会社および投資先企業を除く単体)</p>
+                    </div>
+                </div>
+                <div class="__company-item wow fadeIn">
+                    <p class="_c-ttl">
+                        単体業績
+                    </p>
+                    <div class="_c-ctn is-pc">
+                        <p>令和4年7月期：売上 46億・営業利益 5.5億・経常利益 4.3億 (単体 実績)</p>
+                        <p>令和5年7月期：売上 89億・営業利益 14.7億・経常利益 12.9億 (単体 実績)</p>
+                        <p>令和6年7月期：売上 80億・営業利益 15.0億・経常利益 13.0億 (単体 今期予想)</p>
+                        <p>令和7年7月期：売上100億・営業利益 17.0億・経常利益 15.0億 (単体 来期予想)</p>
+                        <p>令和4年7月期：売上 46億・営業利益 5.5億・経常利益 4.3億 (単体 実績)</p>
+                        <p>令和4年7月期：売上 46億・営業利益 5.5億・経常利益 4.3億 (単体 実績)</p>
+                        <div class="star">
+                            <p class="_star-ttl">
+                                実績は独立監査人の監査報告書にもどづく
+                            </p>
+                        </div>
+                    </div>
+                    <div class="_c-ctn is-sp ">
+                        <p class="pt-16">令和4年7月期 (単体 実績) <br>売上 46億<br>営業利益 5.5億<br>経常利益 4.3億</p>
+                        <p class="pt-16">令和5年7月期 (単体 実績) <br>売上 89億<br>営業利益 14.7億<br>経常利益 12.9億</p>
+                        <p class="pt-16">令和6年7月期 (単体 今期予想) <br>売上 80億<br>営業利益 15.0億<br>経常利益 13.0億</p>
+                        <p class="pt-16">令和7年7月期 (単体 来期予想) <br>売上100億<br>営業利益 17.0億<br>経常利益 15.0億</p>
+
+                        <div class="star">
+                            <p class="_star-ttl">
+                                実績は独立監査人の監査報告書にもどづく
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="__company-item wow fadeIn">
+                    <p class="_c-ttl">
+                        子会社
+                    </p>
+                    <div class="_c-ctn">
+                        <p>株式会社おおきにフロンティア <br class="is-sp"> (不動産保有・賃貸業) 〈100%子会社〉</p>
+                        <p>株式会社九州フロンティア <br class="is-sp"> (不動産保有・賃貸業) 〈今期設立予定〉</p>
+                    </div>
+                </div>
+                <div class="__company-item wow fadeIn">
+                    <p class="_c-ttl">
+                        関連会社
+                    </p>
+                    <div class="_c-ctn">
+                        <p class="is-pc">東邦レマック株式会社 <br class="is-sp"> (靴製造・卸売業) <br class="is-sp"> 〈東証STANDARD
+                            7422：当社は筆頭株主です〉</p>
+                        <p class="is-sp">東邦レマック株式会社 <br class="is-sp"> (靴製造・卸売業) <br class="is-sp"> 〈東証STANDARD 7422〉<br
+                                class="is-sp">〈当社は筆頭株主です〉</p>
+                        <p>株式会社 TOSHIN <br class="is-sp"> (解体業・産業廃棄物収集運搬) <br class="is-sp">〈非上場〉</p>
+                        <p>株式会社 SINGULARIT <br class="is-sp"> (広告・マーケティング・デザイン制作等)</p>
+                        <p>一般社団法人吉野町２丁目老人ホーム<br class="is-sp">（信託受益権に係る信託事務等）</p>
+                    </div>
+                </div>
+                <div class="__company-item wow fadeIn">
+                    <p class="_c-ttl">
+                        免許
+                    </p>
+                    <div class="_c-ctn">
+                        <p>宅地建物取引業東京都知事(3)第91374号</p>
+                        <p>賃貸住宅管理業者国土交通大臣(2)第2344号</p>
+                        <p>不動産特定共同事業東京都知事 第180号</p>
+                    </div>
+                </div>
+                <div class="__company-item wow fadeIn">
+                    <p class="_c-ttl">
+                        顧問弁護士 (50音順)
+                    </p>
+                    <div class="_c-ctn">
+                        <p>光和総合法律事務所 <br class="is-sp"> (港区赤坂4丁目7番15号 陽栄光和ビル)</p>
+                        <p>響総合法律事務所<br class="is-sp"> (中央区築地3-3-2 NEWS築地ビル6階)</p>
+                        <p>日野正晴法律事務所<br class="is-sp">〈初代金融庁長官〉 (千代田区九段南2-6-13 ビラハイツ北の丸5階)</p>
+                        <p>山越総合法律事務所<br class="is-sp"> (千代田区平河町1-7-20 平河町辻田ビル6階)</p>
+                    </div>
+                </div>
+                <div class="__company-item wow fadeIn">
+                    <p class="_c-ttl">
+                        加盟団体
+                    </p>
+                    <div class="_c-ctn">
+                        <p>社団法人全国宅地建物取引業保証協会会員</p>
+                        <p>社団法人東京都宅地建物取引業協会員</p>
+                        <p>一般社団法人リノベーション協議会</p>
+                        <p>東京商工会議所会員</p>
+                        <p>東京都不動産協同組合会員</p>
+                        <p>一般社団法人全日華不動産協会</p>
+                        <p>各種賃貸保証会社代理店</p>
+                    </div>
+                </div>
+                <div class="__company-item wow fadeIn">
+                    <p class="_c-ttl">
+                        著書・刊行物
+                    </p>
+                    <div class="_c-ctn">
+                        <p>『海外投資家に日本の不動産を売る方法』<br class="is-sp">(総合法令出版：amazonにて発売中)</p>
+                        <p>『相続のお守り(不動産相続編)』<br class="is-sp">(総合法令出版：amazonにて発売中)</p>
+
+                    </div>
+                </div>
+                <div class="__company-item wow fadeIn">
+                    <p class="_c-ttl">
+                        海外講演活動セミナー等
+                    </p>
+                    <div class="_c-ctn">
+                        <p>台湾不動産セミナー <br class="is-sp">(2015年11月、台北 W Hotel於)</p>
+                        <p>中国不動産セミナー <br class="is-sp">(2017年10月、北京国家会議中心於)</p>
+                        <p>シンガポール不動産セミナー <br class="is-sp">(2018年8月、シンガポールFort Canning Hotel於)</p>
+                        <p>三門峡プレミアムワールドツアー <br class="is-sp">(2022年6月、河南省三門峡市主催 リモート出演)</p>
+
+                    </div>
+                </div>
+                <div class="__company-item wow fadeIn">
+                    <p class="_c-ttl">
+                        クラファンサイト
+                    </p>
+                    <div class="_c-ctn">
+                        <p>不動産クラファン公式サイト<br class="is-sp">『らくたま』の運営<br class="is-sp">(https://rakutama.jp/)</p>
+                        <div class="star">
+                            <p class="_star-ttl">
+                                ＊『らくたま』は商標登録（商願 2023-013299）されております。<br>
+                                ＊投資家との関係を密にするべく、毎月ファンドを組成しております。
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="__company-item wow fadeIn">
+                    <p class="_c-ttl">
+                        取引金融機関 (50音順)
+                    </p>
+                    <div class="_c-ctn">
+                        <p>朝日信用金庫 / あすか信用組合 / SBIエステートファイナンス (SBI証券グループ) / 大阪協栄信用組合 / オリックス・オリックス銀行 / 香川銀行 / 関西みらいリース /
+                            きらぼし銀行 / 九州リースサービス / 群馬銀行 / JA三井リース / 商工中金 / 湘南信用金庫 / 城南信用金庫 / 城北信用金庫 / 常陽銀行 /SBJ銀行 (新韓銀行) /
+                            昭和リース (SBI証券グループ) / 西武信用金庫 / セゾンファンデックス / 第一勧業信用組合 / 大東京信用組合 / タイヘイ / 台湾第一商業銀行 (東京支店) /
+                            タスキプロス / 千葉興業銀行 / 東和銀行 / 東京シティ信用金庫 / 東京信用金庫 / バンカーズ / 日本住宅無尽 (三菱UFJグループ) / 日本政策金融公庫 / 東日本銀行
+                            / 北陸銀行 / みずほ銀行 / 三井住友銀行 / 三井住友ローン＆ファイナンス / 三菱UFJ銀行 / ミレ信用組合 / 横浜幸銀信用組合 / ロードスターキャピタル</p>
+                        <p>以上、40社超える金融機関と取引</p>
+                    </div>
+                </div>
+                <div class="__company-item wow fadeIn">
+                    <p class="_c-ttl">
+                        社内有資格者
+                    </p>
+                    <div class="_c-ctn">
+                        <p>朝日信用金庫 / あすか信用組合 / SBIエステートファイナンス (SBI証券グループ) / 大阪協栄信用組合 / オリックス・オリックス銀行 / 香川銀行 / 関西みらいリース /
+                            きらぼし銀行 / 九州リースサービス / 群馬銀行 / JA三井リース / 商工中金 / 湘南信用金庫 / 城南信用金庫 / 城北信用金庫 / 常陽銀行 /SBJ銀行 (新韓銀行) /
+                            昭和リース (SBI証券グループ) / 西武信用金庫 / セゾンファンデックス / 第一勧業信用組合 / 大東京信用組合 / タイヘイ / 台湾第一商業銀行 (東京支店) /
+                            タスキプロス / 千葉興業銀行 / 東和銀行 / 東京シティ信用金庫 / 東京信用金庫 / バンカーズ / 日本住宅無尽 (三菱UFJグループ) / 日本政策金融公庫 / 東日本銀行
+                            / 北陸銀行 / みずほ銀行 / 三井住友銀行 / 三井住友ローン＆ファイナンス / 三菱UFJ銀行 / ミレ信用組合 / 横浜幸銀信用組合 / ロードスターキャピタル</p>
+                        <p>以上、40社超える金融機関と取引</p>
+                    </div>
+                </div>
+                <div class="__company-item wow fadeIn">
+                    <p class="_c-ttl">
+                        社内有資格者
+                    </p>
+                    <div class="_c-ctn">
+                        <p>宅地建物取引士 10名</p>
+                        <p>ビル経営管理士 4名</p>
+                        <p>不動産コンサルティングマスター 1名</p>
+                        <p>司法書士 1名</p>
+                        <p>行政書士 1名</p>
+                        <p>防火管理者 4名</p>
+                    </div>
+                </div>
+            </div>
+        </div>      
+
+<?php get_footer() ?>
