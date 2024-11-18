fac83285f5faec39f2faf0d17208ee6e71976656
pandablue0809 <bluep0415@gmail.com>
2024-11-18 15:51:41 +0900

    add about page

---
+++ page-about.php
@@ -0,0 +1,104 @@
+<?php get_header()?>
+
+
+<div class="p-main" id="about">
+        <div class="p-nav">
+            <div class="__container">
+                <a href="#">ホーム &nbsp;&nbsp;&nbsp; ></a>
+                <a href="#">企業情報</a>
+            </div>
+        </div>
+        <div class="p-pagettl">
+            <div class="__container">
+
+                <p class="en-ttl wow fadeInUp">
+                    ABOUT US
+                </p>
+                <h3 class="jp-ttl wow fadeInUp">
+                    私たちについて
+                </h3>
+                <figure class="__absolute wow fadeInRight is-pc">
+                    <img src="<?php echo get_template_directory_uri() ?>/assets/img/about/img-ttl.png" alt="">
+                </figure>
+                <figure class="__absolute wow fadeInRight is-sp">
+                    <img src="<?php echo get_template_directory_uri() ?>/assets/img/real_estate/img-absoulute@2x.png" alt="">
+                </figure>
+            </div>
+        </div>
+        <div class="p-pageslider wow fadeInUp">
+            <figure>
+                <img src="<?php echo get_template_directory_uri() ?>/assets/img/about/img-slideritem_1.jpg" alt="">
+            </figure>
+            <figure>
+                <img src="<?php echo get_template_directory_uri() ?>/assets/img/about/img-slideritem_1.jpg" alt="">
+            </figure>
+            <figure>
+                <img src="<?php echo get_template_directory_uri() ?>/assets/img/about/img-slideritem_1.jpg" alt="">
+            </figure>
+            <figure>
+                <img src="<?php echo get_template_directory_uri() ?>/assets/img/about/img-slideritem_1.jpg" alt="">
+            </figure>
+        </div>
+        <div class="p-ourvision">
+            <div class="__container">
+                <div class="__vision-item wow fadeInUp">
+                    <h3 class="__vision-item-ttl">
+                        Mission
+                    </h3>
+                    <p class="__vision-item-ctn">
+                        ミッションについて入ります。ミッションについて入ります。ミッションについて入ります。<br>ミッションについて入ります。ミッションについて入ります。ミッションについて入ります。
+                    </p>
+                </div>
+                <div class="__vision-item wow fadeInUp">
+                    <h3 class="__vision-item-ttl">
+                        Vision
+                    </h3>
+                    <p class="__vision-item-ctn">
+                        ミッションについて入ります。ミッションについて入ります。ミッションについて入ります。<br>ミッションについて入ります。ミッションについて入ります。ミッションについて入ります。
+                    </p>
+                </div>
+                <div class="__vision-item wow fadeInUp">
+                    <h3 class="__vision-item-ttl">
+                        Value
+                    </h3>
+                    <p class="__vision-item-ctn">
+                        ミッションについて入ります。ミッションについて入ります。ミッションについて入ります。<br>ミッションについて入ります。ミッションについて入ります。ミッションについて入ります。
+                    </p>
+                </div>
+                <figure class="__absolute wow fadeInLeft">
+                    <img src="<?php echo get_template_directory_uri() ?>/assets/img/about/img-absolute.png" alt="">
+                </figure>
+            </div>
+        </div>
+        <div class="p-strong">
+            <div class="__container wow fadeInUp">
+                <!-- <figure class="__strong-img">
+                    <img src="<?php echo get_template_directory_uri() ?>/assets/img/about/img-strong.jpg" alt="">
+                </figure> -->
+                <div class="__strong-ctn">
+                    <h4 class="__ctn-ttl">
+                        多様性が生む成長の力
+                    </h4>
+                    <p class="__ctn-desc">
+                        フロンティアグループは、経歴や年齢に関係なく、自分の意見を自由に表現できる環境を提供します。多様な人材が活躍し、個々の成長と企業の発展を共に追求する文化が根付いています。
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="p-message">
+            <div class="__container  wow fadeInUp">
+                <div class="__message-ctn">
+                    <h4 class="__ctn-ttl">
+                        代表様メッセージタイトル
+                    </h4>
+                    <p class="__ctn-desc">
+                        フロンティアグループは、経歴や年齢に関係なく、自分の意見を自由に表現できる環境を提供します。多様な人材が活躍し、個々の成長と企業の発展を共に追求する文化が根付いています。
+                    </p>
+                </div>
+                <figure class="__ctn-img">
+                    <img src="<?php echo get_template_directory_uri() ?>/assets/img/about/img-seo.jpg" alt="">
+                </figure>
+            </div>
+        </div>
+
+<?php get_footer() ?>
