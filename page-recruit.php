4123356c292debd8d4637d73f85b574f496436c7
pandablue0809 <bluep0415@gmail.com>
2024-11-18 16:16:37 +0900

    add recruit page

---
+++ page-recruit.php
@@ -0,0 +1,89 @@
+<?php get_header()?>
+
+<div class="p-main" id="recruit">
+        <div class="p-nav">
+            <div class="__container">
+                <a href="#">Home &nbsp;&nbsp;&nbsp; ></a>
+                <a href="#">採用情報</a>
+            </div>
+        </div>
+        <div class="p-pagettl">
+            <div class="__container">
+
+                <p class="en-ttl wow fadeInUp">
+                    RECRUTE
+                </p>
+                <h3 class="jp-ttl wow fadeInUp">
+                    採用情報
+                </h3>
+                <figure class="__absolute wow fadeInRight is-pc">
+                    <img src="<?php echo get_template_directory_uri() ?>/assets/img/about/img-ttl.png" alt="">
+                </figure>
+                <figure class="__absolute wow fadeInRight is-sp">
+                    <img src="<?php echo get_template_directory_uri() ?>/assets/img/real_estate/img-absoulute@2x.png" alt="">
+                </figure>
+            </div>
+        </div>
+
+        <div class="p-intro">
+            <div class="__intro-ctn wow fadeInLeft">
+                <h4>会社風土</h4>
+                <p>国内外の企業に対するM&フロンティアグループでは、経歴や年齢に関わらず、全員が経営陣や上司に対して意見を堂々と述べることができる風土があります。多様な価値観を尊重し、個々の成長と企業の目標に向けて共に歩む文化が根付いています。10代から80代までの社員や、外国籍、異業種からの転職者も活躍中です。充実した産休制度と残業なしの環境で、誰もがいきいきと働ける職場づくりに力を入れています
+                </p>
+            </div>
+        </div>
+
+
+        <div class="p-career">
+            <div class="__container">
+                <div class="__career-ttl wow fadeIn">
+                    <h4>キャリア採用職種 </h4>
+                    <p class="__ttl-date">
+                        (2023年 9月時点)
+                    </p>
+                </div>
+                <div class="__career-main">
+                    <div class="__career-item wow fadeIn">
+                        <p class="__item-ttl">
+                            勤務地
+                        </p>
+                        <div class="__item-ctn">
+                            <p>東京本社<br>東京都千代田区外神田5-2-5 フロンティア外神田ビル1階・5階</p>
+                            <p class="star">＊大阪支店では募集しておりません</p>
+                        </div>
+                    </div>
+                    <div class="__career-item wow fadeIn">
+                        <p class="__item-ttl">
+                            勤務時間
+                        </p>
+                        <div class="__item-ctn">
+                            <p>9:30～18:30 (残業は殆どありません。)</p>
+                        </div>
+                    </div>
+                    <div class="__career-item wow fadeIn">
+                        <p class="__item-ttl">
+                            休日・休暇
+                        </p>
+                        <div class="__item-ctn">
+                            <p>週休２日制・祝日、夏季休暇 (3日)、年末年始</p>
+                            <p>＜年間休日約120日＞</p>
+                            <p class="star">＊不動産部営業社員は第１土曜日出社</p>
+                        </div>
+                    </div>
+                    <div class="__career-item wow fadeIn">
+                        <p class="__item-ttl">
+                            待遇・福利厚生
+                        </p>
+                        <div class="__item-ctn">
+                            <p>社会保険・厚生年金完備</p>
+                            <p>通勤手当・資格取得手当</p>
+                            <p>産休制度(消化率100％)</p>
+                            <p>終身雇用(定年退職無)</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+<?php get_footer() ?>
