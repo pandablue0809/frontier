9f827ef6fdee39dd9d81496f41ae998a55ea208b
pandablue0809 <bluep0415@gmail.com>
2024-11-18 15:45:20 +0900

    add footer

---
+++ footer.php
@@ -0,0 +1,85 @@
+<div class="c-footer">
+        <div class="p-link">
+            <div class="__container">
+                <div class="__link-item wow fadeInUp">
+                    <div class="__link-ttl">
+                        <h3 class="en-ttl">
+                            CORPORATE
+                        </h3>
+                        <p class="jp-ttl">
+                            企業情報
+                        </p>
+                    </div>
+                    <a href="#" class="__link-ctn">
+                        Learn More
+                    </a>
+                </div>
+                <div class="__link-item wow fadeInUp">
+                    <div class="__link-ttl">
+                        <h3 class="en-ttl">
+                            CONTACT
+                        </h3>
+                        <p class="jp-ttl">
+                            お問い合わせ
+                        </p>
+                    </div>
+                    <a href="#" class="__link-ctn">
+                        Contact us
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="p-footer">
+            <div class="__container">
+                <div class="__link-wraps">
+                    <div class="__link-wrap is-pc">
+                        <a href="#" class="__logo">株式会社フロンティアグループ</a>
+                        <p>〒101-0021<br>東京都千代田区外神田5-2-5 フロンティア外神田ビル
+                        </p>
+                        <a href="tel:03-5817-8933" class="tel">TEL：03-5817-8933</a>
+                        <a href="tel:03-5817-8922" class="tel">FAX：03-5817-8922</a>
+                    </div>
+                    <div class="__link-wrap">
+                        <a href="#">Home</a>
+                        <a href="#">About us</a>
+                        <a href="#">企業ドメイン</a>
+                        <a href="#">&nbsp;•&nbsp;不動産事業</a>
+                        <a href="#">&nbsp;•&nbsp;不動産クラウドファンディグ</a>
+                        <a href="#">&nbsp;•&nbsp;M&Aアドバイザリー事業</a>
+                        <a href="#">所有不動産一覧</a>
+                    </div>
+                    <div class="__link-wrap">
+                        <a href="#">NEWS</a>
+                        <a href="#">IR情報</a>
+                        <a href="#">採用情報</a>
+                        <a href="#">企業情報</a>
+                        <a href="#">お問い合わせ</a>
+                        <a href="#" class="site_map">サイトマップ</a>
+                    </div>
+                    <div class="__link-wrap is-sp ">
+                        <a href="#" class="__logo">株式会社フロンティアグループ</a>
+                        <p>〒101-0021<br>東京都千代田区外神田5-2-5<br>フロンティア外神田ビル
+                        </p>
+                        <a href="tel:03-5817-8933" class="tel">TEL：03-5817-8933</a>
+                        <a href="tel:03-5817-8922" class="tel">FAX：03-5817-8922</a>
+                    </div>
+
+                </div>
+                <p class="__copyright">© Frontier Group, Inc. All Rights Reserved.</p>
+            </div>
+
+        </div>
+    </div>
+
+</body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="<?php echo get_template_directory_uri() ?>/assets/js/slick.js"></script>
+<script src="<?php echo get_template_directory_uri() ?>/assets/js/wow.js"></script>
+<script src="<?php echo get_template_directory_uri() ?>/assets/js/custom.js"></script>
+<script>
+    new WOW().init();
+</script>
+
+</html>
+
+</html>
