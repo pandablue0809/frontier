f7358234857ed34b7217de89b4dca05d3df361c5
pandablue0809 <bluep0415@gmail.com>
2024-11-18 16:13:23 +0900

    add policy page

---
+++ page-policy.php
@@ -0,0 +1,122 @@
+<?php get_header()?>
+
+<div class="p-main" id="policy">
+        <div class="p-nav">
+            <div class="__container">
+                <a href="#">Home &nbsp;&nbsp;&nbsp; ></a>
+                <a href="#">プライバシーポリシー</a>
+            </div>
+        </div>
+        <div class="p-pagettl">
+            <div class="__container">
+
+                <p class="en-ttl wow fadeIn">
+                    PRIVACY POLICY
+                </p>
+                <h3 class="jp-ttl wow fadeIn">
+                    プライバシーポリシー
+                </h3>
+                <figure class="__absolute wow fadeInRight is-pc">
+                    <img src="<?php echo get_template_directory_uri() ?>/assets/img/about/img-ttl.png" alt="">
+                </figure>
+                <figure class="__absolute wow fadeInRight is-sp">
+                    <img src="<?php echo get_template_directory_uri() ?>/assets/img/real_estate/img-absoulute@2x.png" alt="">
+                </figure>
+            </div>
+        </div>
+
+        <div class="p-policy">
+            <div class="__container">
+                <div class="__policy-item wow fadeIn">
+                    <h5 class="__item-ttl">
+                        01 個人情報の取り扱い
+                    </h5>
+                    <p class="__item-ctn">
+                        株式会社フロンティアグループ（以下、「当社」とします）は、個人情報の取り扱いに関して、以下の通り規定します。当社で扱う個人情報は関係法規及び当社規定を遵守して、取得及び利用を適正に行い保護の維持に努めてまいります。
+                    </p>
+                </div>
+                <div class="__policy-item  wow fadeIn">
+                    <h5 class="__item-ttl">
+                        02 個人情報の取得
+                    </h5>
+                    <p class="__item-ctn">
+                        当社は個人情報の収集を、適法かつ公正な手段により行います。収集した個人情報の利用は、収集目的の範囲内で行い、原則として本人の了解なく目的外に利用することや第三者へ提供することはありません。
+                    </p>
+                </div>
+                <div class="__policy-item  wow fadeIn">
+                    <h5 class="__item-ttl">
+                        03 個人情報の利用
+                    </h5>
+                    <p class="__item-ctn">
+                        サービス提供に関する各種手続きおよび取り次ぎのため、<br>
+                        当社の人材紹介事業に係わる業務での、<br>
+                        ①企業への中途採用候補者の紹介と紹介先での採用選考業務のため、そして②紹介者との通常連絡のため<br>
+                        当社の不動産事業における売買・媒介業務のため<br>
+                        お問い合わせの対応のため<br>
+                        市場動向分析（市場調査・データ分析・アンケート等の実施）による商品開発やサービス向上のため<br>
+                        サービス（商品、情報等）提供のための郵便物、電話、E-mail等による営業活動のため<br>
+                        その他、上記の利用目的に付随する目的のため
+                    </p>
+                </div>
+                <div class="__policy-item  wow fadeIn">
+                    <h5 class="__item-ttl">
+                        04 個人情報の利用停止について
+                    </h5>
+                    <p class="__item-ctn">
+                        当社が保有する個人情報について、本人から訂正、削除、利用停止が求められた場合は適切に対処いたします。
+                    </p>
+                </div>
+                <div class="__policy-item wow fadeIn">
+                    <h5 class="__item-ttl">
+                        05 個人情報保護方針の変更について
+                    </h5>
+                    <p class="__item-ctn">
+                        当社は必要に応じて、個人情報保護方針を変更する事があります。個人情報保護方針を変更した場合には、本サイトに掲載します。
+                    </p>
+                </div>
+                <div class="__policy-item wow fadeIn">
+                    <h5 class="__item-ttl">
+                        06 お問い合わせ
+                    </h5>
+                    <p class="__item-ctn">
+                        個人情報保護方針または取扱いのお問合せに関しては当社までご連絡ださい。
+                    </p>
+                </div>
+                <div class="__policy-item wow fadeIn">
+                    <h5 class="__item-ttl">
+                        07 利用規約
+                    </h5>
+                    <p class="__item-ctn">
+                        本サイトをご利用になる前に、以下の利用条件をお読みください。本サイトを利用になる事により、本利用条件を読んだ上で合意したものとみなされます。本サイトの資料は当社のサービスをご理解いただくための一般情報です。よってこれらの情報のみに基づき行動されるべきではありません。また取扱している情報が最新情報でなく、通知なく変更されることがあります。
+                    </p>
+                </div>
+                <div class="__policy-item wow fadeIn">
+                    <h5 class="__item-ttl">
+                        08 著作権
+                    </h5>
+                    <p class="__item-ctn">
+                        本サイトに掲載しているすべての情報（デザイン、コンテンツ、校正、画像、文章など）の著作権は、当社に帰属します。無断転載、複製、転用、改変等を行うことを禁止致します。
+                    </p>
+                </div>
+                <div class="__policy-item wow fadeIn">
+                    <h5 class="__item-ttl">
+                        09 合意管轄裁判所
+                    </h5>
+                    <p class="__item-ctn">
+                        本規約は日本法を準拠法として解釈適用されるものとします。本サイトの利用あるいは関連して発生した紛争は、当社を管轄する裁判所を合意管轄裁判所とします。
+                    </p>
+                </div>
+                <div class="__policy-item wow fadeIn">
+                    <h5 class="__item-ttl">
+                        10 免責事項
+                    </h5>
+                    <p class="__item-ctn">
+                        本サイトにアクセスをしたため、利用者がいかなる被害や損害を被ったとしても、当社ではその一切の責任を負いません。また、本サイトを通じてアクセスする他のサイトについても一切の責任を負いません。
+                        また、当社ではサービスの内容を変更、停止または中止したことで生じた損害の一切の責任を負わないものとします。尚、当社はいかなる場合でも本サイトの内容保証（情報の正確性、有用性、確実性、安全性等）は一切致しません。
+                    </p>
+                </div>
+            </div>
+        </div>
+
+
+<?php get_footer() ?>
