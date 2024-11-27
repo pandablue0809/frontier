a05b94674acb59329cbe32a3a4b591acca3ffe2a
pandablue0809 <bluep0415@gmail.com>
2024-11-27 18:42:59 +0900

    add front page

---
+++ front-page.php
@@ -0,0 +1,264 @@
+<?php get_header()?>
+
+<div class="p-main" id="top">
+        <div class="p-fv">
+            <div class="__container is-pc">
+                <h2>あなた<span>と</span><strong>共</strong><span>に、</span></h2>
+                <h2 class="h2_sec"><b>豊かな未来<span>を</span>デザイン</b></h2>
+                <h3>新たな<span>フロンティア</span>を今ここから。</h3>
+            </div>
+            <div class="__container is-sp wow fadeInLeft">
+                <h2>あなたと共に、</h2>
+                <h2>豊かな未来を</h2>
+                <h2>デザイン</h2>
+                <h3 class="mt-40">新たな、</h3>
+                <h3>フロンティアを今ここから。</h3>
+            </div>
+        </div>
+
+        <div class="p-about">
+            <div class="__container">
+                <div class="p-about-ttl wow fadeInUp">
+                    <h3 class="en-ttl">
+                        About us
+                    </h3>
+                    <p class="jp-ttl is-pc">
+                        フロンティアグループについて
+                    </p>
+                </div>
+                <div class="p-about-main wow fadeInLeft">
+                    <p class="__main-ctn">
+                        私たちは、お客様のコンシェルジュ。<br>
+                        財産・資産管理業務のプロフェッショナルです
+                        <span>長年の実績と信頼で選ばれ続ける、財産・資産管理のプロフェッショナル。お客様の安心を第一に、未来をサポートします。</span>
+                    </p>
+                    <a href="#" class="__main-btn">
+                        Learn More
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="p-domains">
+            <div class="__container">
+                <div class="p-domains-ttl wow fadeInUp">
+                    <h3 class="en-ttl">
+                        BUSINESS DOMAIN
+                    </h3>
+                    <p class="jp-ttl">
+                        事業ドメイン
+                    </p>
+                </div>
+                <div class="p-domains-main wow fadeInUp">
+                    <div class="__main-item is-pc bg-1">
+                        <p class="__item-ttl">不動産事業</p>
+                        <p class="__item-ctn">資産価値を高める<br>不動産のプロフェッショナル</p>
+                    </div>
+                    <div class="__main-item is-pc bg-2">
+                        <p class="__item-ttl">不動産<br>クラウドファンディング</p>
+                        <p class="__item-ctn">楽に、楽しく、安心してできる<br>不動産クラファン</p>
+                    </div>
+                    <div class="__main-item is-pc bg-3">
+                        <p class="__item-ttl">M &A<br>投資育成</p>
+                        <p class="__item-ctn">未来を共に創る<br>M&Aの最前線パートナー</p>
+                    </div>
+                    <div class="__main-item is-pc bg-4">
+                        <p class="__item-ttl">再生エネルギー事業</p>
+                        <p class="__item-ctn">太陽光発電を中心とした、<br>持続可能なエネルギーの発掘。</p>
+                    </div>
+                    <!-- <div class="__main-item">
+                        <p class="__item-ttl">再生エネルギー事業 </p>
+                        <p class="__item-ctn">太陽光発電を中心とした、 <br>持続可能なエネルギーの発掘。</p>
+                    </div>
+                    <div class="__main-item">
+                        <p class="__item-ttl">再生エネルギー事業 </p>
+                        <p class="__item-ctn">太陽光発電を中心とした、 <br>持続可能なエネルギーの発掘。</p>
+                    </div> -->
+
+                    <div class="__main-item-sp is-sp bg-1">
+                        <p class="__item-ttl">不動産事業</p>
+                        <p class="__item-ctn">
+                            不動産を資産から運用へ導くアセットコンサルティング、中古物件のリニューアル・再販を行う再生事業、収益性の高い物件を管理する買収・賃貸事業を展開。各部門が連携し、資産価値を最大化するソリューションを提供します。
+                        </p>
+                    </div>
+                    <div class="__main-item-sp is-sp bg-2">
+                        <p class="__item-ttl">不動産<br>クラウドファンディング</p>
+                        <p class="__item-ctn">
+                            「らくたま」は、1万円から始められる不動産クラウドファンディングです。スマホやパソコンで簡単に投資ができ、不動産のプロが厳選した物件に投資するだけでOK。銀行預金より高いリターンを期待でき、リスクを抑えた安心の運用が可能です。配当金を受け取るだけで、手軽に資産運用がスタートできます。
+                        </p>
+                    </div>
+                    <div class="__main-item-sp is-sp bg-3">
+                        <p class="__item-ttl">M&A<br>投資育成</p>
+                        <p class="__item-ctn">
+                            国内外の企業に対するM&A支援を提供し、合併・買収や事業譲渡を通じて経営課題の解決をサポートします。豊富な実績と専門的な知識を活かし、最適な戦略を提案。さらに、アドバイザーとしてだけでなく、リスクを伴う自己投資にも積極的に取り組み、企業成長を支援しています。
+                        </p>
+                    </div>
+                    <div class="__main-item-sp is-sp bg-4">
+                        <p class="__item-ttl">再生エネルギー事業</p>
+                        <p class="__item-ctn">
+                            太陽光発電を中心とした、持続可能なエネルギーの発掘。
+                        </p>
+                    </div>
+                    <a href="#" class="__domain-more is-sp">Contact us</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="p-portfolio">
+            <div class="__container wow fadeInUp">
+                <div class="p-portfolio-ttl">
+                    <h3 class="en-ttl">
+                        OWNED ASSET PORTFOLIO
+                    </h3>
+                    <p class="jp-ttl">
+                        所有不動産一覧
+                    </p>
+                </div>
+                <div class="p-portfolio-main">
+                    <div class="__main-slider">
+                        <figure class="portfolio_slider-item">
+                            <img src="<?php echo get_template_directory_uri() ?>/assets/img/owned/HIRAI FIVE.png" alt="" class="slider-item">
+                        </figure>
+                        <figure class="portfolio_slider-item">
+                            <img src="<?php echo get_template_directory_uri() ?>/assets/img/owned/Asakusa Six.png" alt="" class="slider-item">
+                        </figure>
+                        <figure class="portfolio_slider-item">
+                            <img src="<?php echo get_template_directory_uri() ?>/assets/img/owned/HIROKOUJI BLUE BLDG.png" alt="" class="slider-item">
+                        </figure>
+                        <figure class="portfolio_slider-item">
+                            <img src="<?php echo get_template_directory_uri() ?>/assets/img/owned/YKビル.png" alt="" class="slider-item">
+                        </figure>
+                        <figure class="portfolio_slider-item">
+                            <img src="<?php echo get_template_directory_uri() ?>/assets/img/owned/アソルティ末広町.png" alt="" class="slider-item">
+                        </figure>
+                        <figure class="portfolio_slider-item">
+                            <img src="<?php echo get_template_directory_uri() ?>/assets/img/owned/ウェルフラット氷川台.png" alt="" class="slider-item">
+                        </figure>
+                        <figure class="portfolio_slider-item">
+                            <img src="<?php echo get_template_directory_uri() ?>/assets/img/owned/シャルムハイツ.png" alt="" class="slider-item">
+                        </figure>
+                        <figure class="portfolio_slider-item">
+                            <img src="<?php echo get_template_directory_uri() ?>/assets/img/owned/ジブリィ三鷹.png" alt="" class="slider-item">
+                        </figure>
+                        <figure class="portfolio_slider-item">
+                            <img src="<?php echo get_template_directory_uri() ?>/assets/img/owned/パレスリキ.png" alt="" class="slider-item">
+                        </figure>
+                        <figure class="portfolio_slider-item">
+                            <img src="<?php echo get_template_directory_uri() ?>/assets/img/owned/フロンティア246ビル.png" alt="" class="slider-item">
+                        </figure>
+                        <figure class="portfolio_slider-item">
+                            <img src="<?php echo get_template_directory_uri() ?>/assets/img/owned/生田駅前ハイツ.png" alt="" class="slider-item">
+                        </figure>
+                        <figure class="portfolio_slider-item">
+                            <img src="<?php echo get_template_directory_uri() ?>/assets/img/owned/銀座グレートビル.png" alt="" class="slider-item">
+                        </figure>
+                    </div>
+
+                </div>
+                <div class="btn-group">
+                    <a href="#" class="portfolio_more">View More</a>
+                    <div class="__slider-controll is-pc">
+                        <a class="__slider-before" href="#">
+                            <img src="<?php echo get_template_directory_uri() ?>/assets/img/ico-sliderback.png" alt="">
+                        </a>
+                        <a class="__slider-next" href="#">
+                            <img src="<?php echo get_template_directory_uri() ?>/assets/img/ico-slidernext.png" alt="">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="p-news">
+            <div class="__container">
+                <div class="p-new-ttl wow fadeInUp">
+                    <h3 class="en-ttl">
+                        NEWS
+                    </h3>
+                    <p class="jp-ttl is-pc">
+                        ニュース
+                    </p>
+                </div>
+                <div class="p-news-main">
+                    <div class="__news-items wow fadeInRight">
+                        <div class="__news-item">
+                            <p class="__news-date">2024. 09 21</p>
+                            <p class="__news-ttl">「らくたま」の評判と口コミに関するブログ記事のご紹介</p>
+                        </div>
+                        <div class="__news-item">
+                            <p class="__news-date">2024. 09 21</p>
+                            <p class="__news-ttl">「らくたま」の評判と口コミに関するブログ記事のご紹介</p>
+                        </div>
+                        <div class="__news-item">
+                            <p class="__news-date">2024. 09 21</p>
+                            <p class="__news-ttl">「らくたま」の評判と口コミに関するブログ記事のご紹介</p>
+                        </div>
+                        <div class="__news-item">
+                            <p class="__news-date">2024. 09 21</p>
+                            <p class="__news-ttl">「らくたま」の評判と口コミに関するブログ記事のご紹介</p>
+                        </div>
+                    </div>
+                    <a href="#" class="__news-btn">
+                        View More
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="p-recruit">
+            <div class="__container wow fadeInUp">
+                <div class="p-recruit-ttl">
+                    <h3 class="en-ttl">
+                        RECRUIT
+                    </h3>
+                    <p class="jp-ttl is-pc">
+                        採用情報
+                    </p>
+                </div>
+                <div class="p-recruit-main">
+                    <div class="__recruit-ctn">
+                        <p class="__ctn-ttl">
+                            多様性が生む成長の力
+                        </p>
+                        <p class="__ctn-desc">
+                            フロンティアグループは、経歴や年齢に関係なく、自分の意見を自由に表現できる環境を提供します。多様な人材が活躍し、個々の成長と企業の発展を共に追求する文化が根付いています。
+                        </p>
+                    </div>
+                    <a href="#" class="__recruit-btn">Learn More</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="p-irinfo">
+            <div class="__container wow fadeInUp">
+                <div class="p-irinfo-ttl">
+                    <h3 class="en-ttl is-pc">
+                        IR INFO
+                    </h3>
+                    <p class="jp-ttl">
+                        IR情報
+                    </p>
+                </div>
+                <div class="p-irinfo-main">
+                    <p class="__irinfo-ctn">
+                        IRニュース、IR情報、株式情報についてご覧いただけます。
+                    </p>
+                    <a href="#" class="__irinfo-btn">Learn More</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="p-badge">
+            <div class="__container badge-slider">
+                <figure>
+                    <img src="<?php echo get_template_directory_uri() ?>/assets/img/img-badge_1.jpg" alt="">
+                </figure>
+                <figure>
+                    <img src="<?php echo get_template_directory_uri() ?>/assets/img/img-badge_2.gif" alt="">
+                </figure>
+            </div>
+        </div>
+    </div>
+
+
+<?php get_footer() ?>
